42e72038105e1175fe1e7673a89dedb1f46a012c
Mettfan <cesaryannick6@gmail.com>
2022-10-05 22:10:48 -0500

    adding schedules

--- src/Components/Landing/Extra/Extra.js
+++ src/Components/Landing/Extra/Extra.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Extra.css'
-
+import { useNavigate } from 'react-router-dom'
 function Extra(props) {
+    let nav = useNavigate()
+    let goto = (url) => {
+        nav(url)
+        window.scrollTo(0, 0)
+    }
     return ( <>
 
-        <div className='extraContainer'>
+        <div className='extraContainer' onClick={() => {goto(props.redirect)}}>
             
             <img className='extraImage' src={props.image} />
             <div className='extraText'>
